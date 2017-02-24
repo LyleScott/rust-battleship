f8e9ead7dc96fa20aded6d73e7809cbedc94286d
Lyle Scott, III <lyle@digitalfoo.net>
2017-02-23 23:00:38 -0500

    add a very nieve implementation of placing pieces on the board. But i got through it...

--- src/main.rs
+++ src/main.rs
@@ -34,24 +34,63 @@ struct Board {
 impl Board {
     fn initialize(&mut self) {
         // Constraints.
-        let max_height = 9;
         let max_width = 10;
-
-        //self.ships[0].x = 3;
+        let max_height = 9;
+        let mut skip: bool;
 
         for ship in self.ships.iter_mut() {
-            while true {
+            loop {
+                // Pick a starting point.
+                let w = rand::thread_rng().gen_range(0, max_width - 1);
+                let h = rand::thread_rng().gen_range(0, max_height - 1);
+
+                // Check if that space is taken.
+                if self.spaces[h as usize][w as usize] != '-' {
+                    continue
+                }
+
                 // true = up/down, false = left/right
                 let orientation: bool = rand::thread_rng().gen();
 
-                // Pick a starting point.
-                let x = rand::thread_rng().gen_range(0, 9);
-                let y = rand::thread_rng().gen_range(0, 10);
-
-                ship.x = x;
-                ship.y = y;
-                break;
-                // TODO: finish.
+                if orientation {
+                    // Make sure the points are in bounds.
+                    if h + ship.len >= max_height {
+                        continue
+                    }
+                    skip = false;
+                    for i in h..ship.len + h {
+                        if self.spaces[i as usize][w as usize] != '-' {
+                            skip = true;
+                            continue
+                        }
+                    }
+                    if skip {
+                        continue
+                    }
+                    for i in h..ship.len + h {
+                        self.spaces[i as usize][w as usize] = ship.name.chars().nth(0).unwrap();
+                    }
+                    break;
+                } else {
+                    // Make sure the points are in bounds.
+                    if w + ship.len >= max_width {
+                        continue
+                    }
+                    skip = false;
+                    for i in w..ship.len + w {
+                        if self.spaces[h as usize][i as usize] != '-' {
+                            skip = true;
+                            continue
+                        }
+                    }
+                    if skip {
+                        continue
+                    }
+                    for i in w..ship.len + w {
+                        self.spaces[h as usize][i as usize] = ship.name.chars().nth(0).unwrap();
+                    }
+                    break;
+                }
             }
         }
     }
@@ -100,10 +139,10 @@ fn board_factory(name: String) -> Board {
 }
 
 fn main() {
-    let mut computer_board = board_factory("Computer".to_string());
+    //let mut computer_board = board_factory("Computer".to_string());
     let mut human_board = board_factory("Human".to_string());
 
-    human_board.print_board();
     human_board.initialize();
+    human_board.print_board();
 }
 
